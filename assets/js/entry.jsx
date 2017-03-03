49b4bf3a611fb22eceab4fd187ee72fc47a5d812
Sung Kwang David Kim <sungkwangkim@gmail.com>
2017-03-02 17:43:41 -0800

    create temp file to store all html

---
+++ assets/js/entry.jsx
@@ -0,0 +1,12 @@
+// require('../css/main.css');
+// require('../css/david.css');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './header';
+import Test from './test';
+
+ReactDOM.render(
+  <Test />,
+  document.getElementById('wrapper')
+);
---
+++ assets/js/header.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class Header extends React.component {
+  render(){
+    return (
+      <div>
+        <div class="logo">
+          <span class="icon">
+            <img class="cross" src="./images/cross.png" alt=""/>
+          </span>
+        </div>
+        <div class="content">
+          <div class="inner">
+            <h1>Oxnard Korean Church</h1>
+            <p>사랑하는 자여 네 영혼이 잘 됨같이 네가 범사에 잘 되고 <br />
+            강건하기를 내가 간구하노라 (요한삼서 1:2)</p>
+          </div>
+        </div>
+        <nav>
+          <ul>
+            <li><a href="#intro">Service</a></li>
+            <li><a href="#work">Location</a></li>
+            // <li><a href="#about">About</a></li>
+            // <li><a href="#contact">Contact</a></li>
+            // <li><a href="#elements">Elements</a></li>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default Header;
