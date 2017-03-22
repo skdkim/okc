26a140a6f98343b03a6c04638eb830e81aa629e6
Sung Kwang David Kim <sungkwangkim@gmail.com>
2017-03-22 00:29:18 -0700

    change class to className to activate styling

--- assets/js/service.jsx
+++ assets/js/service.jsx
@@ -3,51 +3,51 @@ import React from 'react';
 function Service(props){
   return (
   <article id="intro">
-    <div class="introModal">
-      <h2 class="major">Service Times</h2>
-      <div class="schedule">
-        <div class="days">
-          <div class="day">
+    <div className="introModal">
+      <h2 className="major">Service Times</h2>
+      <div className="schedule">
+        <div className="days">
+          <div className="day">
             <p>Sunday</p>
           </div>
-          <div class="times">
-            <div class="details">
+          <div className="times">
+            <div className="details">
               <p>Morning Service: </p><p>8:00 am</p>
             </div>
-            <div class="details">
+            <div className="details">
               <p>Afternoon Service: </p><p>1:00 pm</p>
             </div>
           </div>
         </div>
 
-        <div class="days">
-          <div class="day">
+        <div className="days">
+          <div className="day">
             <p>Wednesday</p>
           </div>
-          <div class="times">
-            <div class="details">
+          <div className="times">
+            <div className="details">
               <p>Evening Service: </p><p>6:30 pm</p>
             </div>
           </div>
         </div>
 
-        <div class="days">
-          <div class="day">
+        <div className="days">
+          <div className="day">
             <p>Tues - Fri</p>
           </div>
-          <div class="times">
-            <div class="details">
+          <div className="times">
+            <div className="details">
               <p>Dawn Service/Prayer: </p><p>5:30 am</p>
             </div>
           </div>
         </div>
 
-        <div class="days">
-          <div class="day">
+        <div className="days">
+          <div className="day">
             <p>Saturday</p>
           </div>
-          <div class="times">
-            <div class="details">
+          <div className="times">
+            <div className="details">
               <p>Prayer Meeting: </p><p>6:00 am</p>
             </div>
           </div>
