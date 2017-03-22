14cb9932f7ef2ea1418fc75f6d1ed258ff688c10
Sung Kwang David Kim <sungkwangkim@gmail.com>
2017-03-21 23:29:01 -0700

    delete test files

--- assets/js/test2.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Test2(props){
-  return(
-    <h1>Helloo</h1>
-  );
-}
-
-export default Test2;
--- assets/js/test3.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Test3(props){
-  return(
-    <h1>Hellooo</h1>
-  );
-}
-
-export default Test3;
