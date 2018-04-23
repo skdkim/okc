f3e81648bcc08c97930fb5da2794d89dbb7cba67
Sung Kwang David Kim <sungkwangkim@gmail.com>
2018-04-22 23:37:27 -0700

    update sermon

--- assets/js/sermon.jsx
+++ assets/js/sermon.jsx
@@ -9,9 +9,9 @@ function Sermon(props){
     <article id="sermon">
       <div className="introModal">
         <h2 className="major" id="sermon" >Sermon</h2>
-        <iframe classname="location" id="sermonVid" width="560" height="315" 
-          src="https://www.youtube.com/embed/YK0NlFvDc2I?rel=0"
-          frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        <iframe className="location" id="sermonVid" width="560" height="315"
+          src="https://www.youtube.com/embed/3JXTxC9zjBQ?rel=0"
+          frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
       </div>
     </article>
   );
