9e8c75558df1867f08b7f771b733559f9b00e157
Sung Kwang David Kim <sungkwangkim@gmail.com>
2018-05-27 13:49:36 -0700

    udpate sermon video

--- assets/js/scripts/youtubeAPI.js
+++ assets/js/scripts/youtubeAPI.js
@@ -9,7 +9,7 @@ function onYouTubeIframeAPIReady() {
   player = new YT.Player('sermonVid', {
     height: '315',
     width: '560',
-    videoId: 'BT1RIqpVGYc',
+    videoId: 'HjfgzEubOtM',
     playerVars: {
       'rel': 0
     },
