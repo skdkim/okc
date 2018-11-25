57636778d332003519fa9d53b8ca4f6ec4ded51b
Sung Kwang David Kim <sungkwangkim@gmail.com>
2018-11-25 13:53:57 -0800

    update sermon

--- assets/js/scripts/youtubeAPI.js
+++ assets/js/scripts/youtubeAPI.js
@@ -9,7 +9,7 @@ function onYouTubeIframeAPIReady() {
   player = new YT.Player('sermonVid', {
     height: '315',
     width: '560',
-    videoId: 'I03TiLvnApA',
+    videoId: '1ECFk6nlXZA',
     playerVars: {
       'rel': 0
     },
